31ca11b31a7516595599d95943230f0f45eada5e
8090anshumansingh <anshumansinghrajawat.2000@gmail.com>
2022-12-08 15:44:54 +0530

    railway deploy

--- client-side/src/axios.js
+++ client-side/src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //  baseURL: "http://localhost:8800",
-   baseURL: "https://rhythm-back.herokuapp.com/",
+   baseURL: "https://web-production-ef01.up.railway.app",
+  //  baseURL: "https://rhythm-back.herokuapp.com/",
+  // baseURL: "http://localhost:8800",
 });
 
 export default instance;
